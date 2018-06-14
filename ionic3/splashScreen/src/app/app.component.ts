cdb0291ddca0a6cb7628fb538595e0d4adf7f788
Allan Santos <allan.allan.santos@gmail.com>
2018-06-14 10:35:43 -0300

    Splash Screen with spinner screen ok ok

--- ionic3/splashScreen/src/app/app.component.ts
+++ ionic3/splashScreen/src/app/app.component.ts
@@ -6,6 +6,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 import { HomePage } from '../pages/home/home';
 import { ListPage } from '../pages/list/list';
 
+import { timer } from 'rxjs/observable/timer';
+
 @Component({
   templateUrl: 'app.html'
 })
@@ -15,6 +17,8 @@ export class MyApp {
   rootPage: any = HomePage;
 
   pages: Array<{title: string, component: any}>;
+  
+  showSplash = true;
 
   constructor(public platform: Platform, public statusBar: StatusBar, public splashScreen: SplashScreen) {
     this.initializeApp();
@@ -29,10 +33,12 @@ export class MyApp {
 
   initializeApp() {
     this.platform.ready().then(() => {
+    
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       this.statusBar.styleDefault();
       this.splashScreen.hide();
+      timer(3000).subscribe(() => this.showSplash = false);
     });
   }
 
