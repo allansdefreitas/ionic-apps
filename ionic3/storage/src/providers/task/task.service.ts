d74b6f22566c2a4612c2f76094fbe923db83c5bc
Allan Santos <allan.allan.santos@gmail.com>
2018-08-31 23:52:42 -0300

    getAll() starting...

--- ionic3/storage/src/providers/task/task.service.ts
+++ ionic3/storage/src/providers/task/task.service.ts
@@ -1,11 +1,37 @@
 import { Injectable } from '@angular/core';
 import { Storage } from '@ionic/storage';
+import { Task } from '../../models/task.model';
 
 @Injectable()
 export class TaskService {
 
-  constructor(storage: Storage){
+  constructor(public storage: Storage){
 
+  }
+
+  getAll(): Promise<Task[]>{
+
+    return this.storage.ready()
+
+      .then( (localForage: LocalForage) => {
+
+        let tasks: Task[] = [];
+
+        return this.storage.forEach( (task: Task, key: string, interationNumber: number) =>{
+          if (key.indexOf('tasks.') > -1 ){
+            tasks.push(task);
+          }
+
+
+        }).then( () => tasks );
+
+
+      }).catch( err => {
+
+        console.log('Erro ao abrir o storage: ', err );
+        return Promise.reject(err);
+
+      });
   }
 
 }
