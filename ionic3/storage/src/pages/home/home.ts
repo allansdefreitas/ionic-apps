b22b2901a8a2e6d348a2048f6872d0a6394867c3
Allan Santos <allan.allan.santos@gmail.com>
2018-09-04 20:20:06 -0300

    problem FIXED :)

--- ionic3/storage/src/pages/home/home.ts
+++ ionic3/storage/src/pages/home/home.ts
@@ -11,7 +11,10 @@ import { Task } from '../../models/task.model';
   selector: 'page-home',
   templateUrl: 'home.html'
 })
+
 export class HomePage {
+
+  tasks: Task[] = [];
 
   constructor(
     public alertCtrl: AlertController,
@@ -20,64 +23,45 @@ export class HomePage {
     public taskService: TaskService
   ) {}
 
-
-  tasks: Task[] = [];
-
-  ionViewDidLoad(){
-
-      this.taskService.getAll(true)
-        .then ( (tasks: Task[]) =>{
-            this.tasks = tasks;
-        });
-
+  ionViewDidLoad() {
+    this.taskService.getAll(true)
+      .then((tasks: Task[]) => {
+        this.tasks = tasks;
+      });
   }
 
-  private onSave(type: string, itemSliding?: ItemSliding, task?: Task): void {
-
-    let title: string = type.charAt(0) + type.substr(1);
+  onSave(type: string, ItemSliding?: ItemSliding, task?: Task): void {
+    let title: string = type.charAt(0).toUpperCase() + type.substr(1);
     let options = {
-      title: '${title} task',
-      itemSliding: itemSliding,
+      title: `${title} task`,
+      itemSliding: ItemSliding,
       type: type
     };
-    this.showAlert(options,task);
+    this.showAlert(options, task);
   }
 
   onDelete(task: Task): void {
-
     this.alertCtrl.create({
-
-      title: 'Do you want to delete ${task.title} task?',
+      title: `Do you want to delete '${task.title}' task?`,
       buttons: [
         {
           text: 'Yes',
           handler: () => {
-            let loading: Loading = this.showLoading('Deleting ${task.title}...');
+            let loading: Loading = this.showLoading(`Deleting ${task.title}...`);
 
             this.taskService.delete(task.id)
-            .then((deleted: boolean) =>{
-              this.tasks.splice(this.tasks.indexOf(task), 1); /* Apaga a task da posição indexOf..apaga uma só ;) */
-              loading.dismiss();
-            });
+              .then((deleted: boolean) => {
+                this.tasks.splice(this.tasks.indexOf(task), 1);
+                loading.dismiss();
+              });
           }
         },
         'No'
       ]
-
     }).present();
   }
-  private showLoading(message?: string): Loading {
 
-    let loading: Loading = this.loadingCtrl.create({
-        content: message || 'please wait....'
-    });
-
-    loading.present();
-
-    return loading;
-  }
-
-  private showAlert(options: {itemSliding: ItemSliding, title: string, type: string}, task?: Task): void{
+  private showAlert(options: {itemSliding: ItemSliding, title: string, type: string}, task?: Task): void {
 
     let alertOptions: AlertOptions = {
       title: options.title,
@@ -86,50 +70,42 @@ export class HomePage {
           name: 'title',
           placeholder: 'Task title'
         }
-       ],
-       buttons: [
-         {
-           text: 'Cancel',
-           role: 'cancel'
-         },
-         {
-           text: 'Save',
-           handler: (data) => {
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel'
+        },
+        {
+          text: 'Save',
+          handler: (data) => {
 
-              let loading: Loading = this.showLoading('Saving %{data.title} task...');
-              let contextTask: Task;
+            let loading: Loading = this.showLoading(`Saving ${data.title} task...`);
+            let contextTask: Task;
 
-              switch(options.title){
+            switch(options.type) {
+              case 'create':
+                contextTask = new Task(data.title);
+                break;
+              case 'update':
+                task.title = data.title;
+                contextTask = task;
+                break;
+            }
 
-                case 'create':
-                  contextTask = new Task(data.title);
-                  break;
+            this.taskService[options.type](contextTask)
+              .then((savedTask: Task) => {
+                if (options.type === 'create') this.tasks.unshift(savedTask);
+                loading.dismiss();
+                if (options.itemSliding) options.itemSliding.close();
+              });
 
-                case 'update':
-                  task.title = data.title;
-                  contextTask = task;
-                  break;
-              }
+          }
+        }
+      ]
+    };
 
-              this.taskService.save(contextTask) /* this.taskService[options.title](contextTask) não pegou */
-                .then((savedTask: Task) => {
-                  if( options.type === 'create') {
-                    this.tasks.unshift(savedTask);
-                  }
-
-                  loading.dismiss();
-
-                  if (options.itemSliding){
-                    options.itemSliding.close();
-                  }
-
-                });
-           }
-         }
-       ]
-    }; /* Fechando alertOptions */
-
-    if (options.type === 'update'){
+    if (options.type === 'update') {
       alertOptions.inputs[0].value = task.title;
     }
 
@@ -137,4 +113,138 @@ export class HomePage {
 
   }
 
+  private showLoading(message?: string): Loading {
+    let loading: Loading = this.loadingCtrl.create({
+      content: message || 'Please wait...'
+    });
+    loading.present();
+    return loading;
+  }
+
 }
+// export class HomePage {
+
+//   tasks: Task[] = [];
+
+//   constructor(
+//     public alertCtrl: AlertController,
+//     public loadingCtrl: LoadingController,
+//     public navCtrl: NavController,
+//     public taskService: TaskService
+//   ) {}
+
+//   ionViewDidLoad(){
+
+//       this.taskService.getAll(true)
+//         .then ( (tasks: Task[]) =>{
+//             this.tasks = tasks;
+//         });
+
+//   }
+
+//   private onSave(type: string, itemSliding?: ItemSliding, task?: Task): void {
+
+//     let title: string = type.charAt(0) + type.substr(1);
+//     let options = {
+//       title: `${title} task`,
+//       itemSliding: itemSliding,
+//       type: type
+//     };
+//     this.showAlert(options,task);
+//   }
+
+//   onDelete(task: Task): void {
+
+//     this.alertCtrl.create({
+
+//       title: `Do you want to delete '${task.title}' task?`,
+//       buttons: [
+//         {
+//           text: 'Yes',
+//           handler: () => {
+//             let loading: Loading = this.showLoading(`Deleting ${task.title}...`);
+
+//             this.taskService.delete(task.id)
+//             .then((deleted: boolean) =>{
+//               this.tasks.splice(this.tasks.indexOf(task), 1); /* Apaga a task da posição indexOf..apaga uma só ;) */
+//               loading.dismiss();
+//             });
+//           }
+//         },
+//         'No'
+//       ]
+
+//     }).present();
+//   }
+//   private showLoading(message?: string): Loading {
+
+//     let loading: Loading = this.loadingCtrl.create({
+//         content: message || 'please wait....'
+//     });
+
+//     loading.present();
+
+//     return loading;
+//   }
+
+//   private showAlert(options: {itemSliding: ItemSliding, title: string, type: string}, task?: Task): void{
+
+//     let alertOptions: AlertOptions = {
+//       title: options.title,
+//       inputs: [
+//         {
+//           name: 'title',
+//           placeholder: 'Task title'
+//         }
+//        ],
+//        buttons: [
+//          {
+//            text: 'Cancel',
+//            role: 'cancel'
+//          },
+//          {
+//            text: 'Save',
+//            handler: (data) => {
+
+//               let loading: Loading = this.showLoading(`Saving ${data.title} task...`);
+//               let contextTask: Task;
+
+//               switch(options.title){
+
+//                 case 'create':
+//                   contextTask = new Task(data.title);
+//                   break;
+
+//                 case 'update':
+//                   task.title = data.title;
+//                   contextTask = task;
+//                   break;
+//               }
+
+//               this.taskService.save(contextTask) /* this.taskService[options.title](contextTask) não pegou */
+//                 .then((savedTask: Task) => {
+//                   if( options.type === 'create') {
+//                     this.tasks.unshift(savedTask);
+//                   }
+
+//                   loading.dismiss();
+
+//                   if (options.itemSliding){
+//                     options.itemSliding.close();
+//                   }
+
+//                 });
+//            }
+//          }
+//        ]
+//     }; /* Fechando alertOptions */
+
+//     if (options.type === 'update'){
+//       alertOptions.inputs[0].value = task.title;
+//     }
+
+//     this.alertCtrl.create(alertOptions).present();
+
+//   }
+
+// }
--- ionic3/storage/src/providers/task/task.service.ts
+++ ionic3/storage/src/providers/task/task.service.ts
@@ -5,70 +5,116 @@ import { Task } from '../../models/task.model';
 @Injectable()
 export class TaskService {
 
-  constructor(public storage: Storage){
+  constructor(
+    public storage: Storage
+  ) {}
 
-  }
-
-  getAll(reverse: boolean): Promise<Task[]>{
+  getAll(reverse?: boolean): Promise<Task[]> {
 
     return this.storage.ready()
-
-      .then( (localForage: LocalForage) => {
+      .then((localForage: LocalForage) => {
 
         let tasks: Task[] = [];
 
-        return this.storage.forEach( (task: Task, key: string, interationNumber: number) =>{
-          if (key.indexOf('tasks.') > -1 ){
+        return this.storage.forEach((task: Task, key: string, iterationNumber: number) => {
+          if (key.indexOf('tasks.') > -1) {
             tasks.push(task);
           }
+        }).then(() => (!reverse) ? tasks : tasks.reverse());
 
-
-        }).then( () => (!reverse) ? tasks : tasks.reverse() ); /* Mostrar sempre as mais recentes no topo da lista */
-
-
-      }).catch( err => {
-
-        console.log('Erro ao abrir o storage: ', err );
-        return Promise.reject(err);
-
+      }).catch((err: Error) => {
+        console.log('Erro ao abrir o storage: ', `${err.name}: `, err.message || err);
+        return Promise.reject(`Erro ao abrir o storage: ${err.name}: ${err.message || err}`);
       });
+
   }
 
-
-  getById(id: number): Promise<Task> {
-
-    return this.storage.get('tasks.${id}'); /* tasks.0928323 */
+  get(id: number): Promise<Task> {
+    return this.storage.get(`tasks.${id}`); // tasks.46589321
   }
 
-  /*  Os dois métodos abaixo fazem a mesma coisa.. ;)
-      A presença dos dois é meramente didática, pois
-      poderia ser apenas um método com o nome save, por exemplo */
-
-
-  save(task: Task): Promise<Task> {
-
-    return this.storage.set('tasks.${task.id}', task);
+  create(task: Task): Promise<Task> {
+    return this.storage.set(`tasks.${task.id}`, task);
   }
 
   update(task: Task): Promise<Task> {
-
-    return this.save(task);
+    return this.create(task);
   }
-
 
   delete(id: number): Promise<boolean> {
-
-    return this.storage.remove('tasks.${id}')
-
-    .then(() => true);
+    return this.storage.remove(`tasks.${id}`)
+      .then(() => true);
   }
-  // delete(id: number): Promise<Task> {
-
-  //     return this.storage.remove(
-  //         this.getById(number).id
-
-  //     )
-
-  // }
 
 }
+
+// @Injectable()
+// export class TaskService {
+
+//   constructor(public storage: Storage){
+
+//   }
+
+//   getAll(reverse: boolean): Promise<Task[]>{
+
+//     return this.storage.ready()
+
+//       .then( (localForage: LocalForage) => {
+
+//         let tasks: Task[] = [];
+
+//         return this.storage.forEach( (task: Task, key: string, interationNumber: number) =>{
+//           if (key.indexOf('tasks.') > -1 ){
+//             tasks.push(task);
+//           }
+
+
+//         }).then( () => (!reverse) ? tasks : tasks.reverse() ); /* Mostrar sempre as mais recentes no topo da lista */
+
+
+//       }).catch( err => {
+
+//         console.log('Erro ao abrir o storage: ', err );
+//         return Promise.reject(err);
+
+//       });
+//   }
+
+
+//   getById(id: number): Promise<Task> {
+
+//     return this.storage.get('tasks.${id}'); /* tasks.0928323 */
+//   }
+
+//   /*  Os dois métodos abaixo fazem a mesma coisa.. ;)
+//       A presença dos dois é meramente didática, pois
+//       poderia ser apenas um método com o nome save, por exemplo */
+
+
+//   save(task: Task): Promise<Task> {
+
+//     return this.storage.set('tasks.${task.id}', task);
+//   }
+
+//   update(task: Task): Promise<Task> {
+
+//     return this.save(task);
+//   }
+
+
+//   delete(id: number): Promise<boolean> {
+
+//     return this.storage.remove('tasks.${id}')
+
+//     .then(() => true);
+//   }
+//   // delete(id: number): Promise<Task> {
+
+//   //     return this.storage.remove(
+//   //         this.getById(number).id
+
+//   //     )
+
+//   // }
+
+// }
