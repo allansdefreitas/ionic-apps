cee95e69e71d31c5294e44eddfd0389c17618aec
Allan Santos <allan.allan.santos@gmail.com>
2018-09-04 18:47:12 -0300

    showAlert para salvar e atualizar tasks

--- ionic3/storage/src/pages/home/home.ts
+++ ionic3/storage/src/pages/home/home.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
-import { NavController, IonicPage, Loading, LoadingController } from 'ionic-angular';
+import { NavController, IonicPage, Loading, LoadingController, ItemSliding, AlertOptions, AlertController } from 'ionic-angular';
+
+
+import { TaskService } from '../../providers/task/task.service';
 
 import { Task } from '../../models/task.model';
-import { TaskService } from '../../providers/task/task.service';
 
 @IonicPage()
 @Component({
@@ -12,6 +14,7 @@ import { TaskService } from '../../providers/task/task.service';
 export class HomePage {
 
   constructor(
+    public alertCtrl: AlertController,
     public loadingCtrl: LoadingController,
     public navCtrl: NavController,
     public taskService: TaskService
@@ -40,6 +43,64 @@ export class HomePage {
     return loading;
   }
 
-  private showAlert()...
+  private showAlert(options: {itemSliding: ItemSliding, title: string, type: string}, task?: Task): void{
+
+    let alertOptions: AlertOptions = {
+      title: options.title,
+      inputs: [
+        {
+          name: 'title',
+          placeholder: 'Task title'
+        }
+       ],
+       buttons: [
+         {
+           text: 'Cancel',
+           role: 'cancel'
+         },
+         {
+           text: 'Save',
+           handler: (data) => {
+
+              let loading: Loading = this.showLoading('Saving %{data.title} task...');
+              let contextTask: Task;
+
+              switch(options.title){
+
+                case 'create':
+                  contextTask = new Task(data.title);
+                  break;
+
+                case 'update':
+                  task.title = data.title;
+                  contextTask = task;
+                  break;
+              }
+
+              this.taskService[options.title](contextTask)
+                .then((savedTask: Task) => {
+                  if( options.type === 'create') {
+                    this.tasks.unshift(savedTask);
+                  }
+
+                  loading.dismiss();
+
+                  if (options.itemSliding){
+                    options.itemSliding.close();
+                  }
+
+                });
+           }
+         }
+       ]
+    }; /* Fechando alertOptions */
+
+    if (options.type === 'update'){
+      alertOptions.inputs[0].value = task.title;
+    }
+
+    this.alertCtrl.create(alertOptions).present();
+
+  }
 
 }
